bb982d5f82ca1514e7c29cfa395f0d17d3312390
George J. WENG <113630673+GtGoodScrub@users.noreply.github.com>
2024-11-16 15:23:03 -0800

    Added a bit more info to about page - GW

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -3,7 +3,8 @@ const About = () => {
         <img src={require("../images/CookBookLogo.png")} height={150} width={200} alt="cookbooklogo"/>
         <h2>About George</h2>
         <img src={require("../images/GW.jpg")} height={200} width={200} alt="My Pic"/>
-        <p>Hello There!  My name is George.  Something something backstory...</p>
+        <p>Hello There!  My name is George.  I'm a student at Douglas College working towards becoming a Software Engineer.</p>
+        <p>Here's my email: georgejw555@gmail.com</p>
 
         <h2>About Junhee</h2>
         <img alt="Junhee's Pic"/>
