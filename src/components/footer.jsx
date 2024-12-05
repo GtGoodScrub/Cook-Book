918d8acd0d0677d7c7e8cdd9141c3e44891ccee6
Junhee <ttl7506@gmail.com>
2024-12-04 21:46:42 -0800

    edit footer and add more data

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,14 +3,16 @@ import "../styles/footer.css";
 
 const Footer = () => {
     return (
-        <footer className="footer">
-            <p>&copy; 2024 Cook Book. All Rights Reserved.</p>
-            <ul className="footer-links">
-                <li><a href="/terms">Terms of Service</a></li>
-                <li><a href="/privacy">Privacy Policy</a></li>
-                <li><a href="/contact">Contact Us</a></li>
-            </ul>
-        </footer>
+        <div className="main-content">
+            <footer className="footer">
+                <p>&copy; 2024 Cook Book. All Rights Reserved.</p>
+                <ul className="footer-links">
+                    <li><a href="/terms">Terms of Service</a></li>
+                    <li><a href="/privacy">Privacy Policy</a></li>
+                    <li><a href="/contact">Contact Us</a></li>
+                </ul>
+            </footer>
+        </div>
     );
 };
 
